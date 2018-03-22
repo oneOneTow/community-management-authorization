5defbb6401f48a18f08807478efdc6941ea96f48
vens <2319687449@qq.com>
2018-03-22 22:57:16 +0800

    GDA-001 add gateway

---
+++ src/main/java/vens/ai/graduate/design/authorization/gateway/FaceRecognitionGateway.java
@@ -0,0 +1,10 @@
+package vens.ai.graduate.design.authorization.gateway;
+
+/**
+ * call other api to achieve face recognize
+ *
+ * @author vens
+ * @date 2018-03-22 22:54
+ **/
+public class FaceRecognitionGateway {
+}
